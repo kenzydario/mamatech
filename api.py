7061519ce06e661f02eebfec23e6ba0cc6d8591a
kenzy <kenzy.sanjaya@tokopedia.com>
2021-04-29 00:31:34 +0700

    first

---
+++ api.py
@@ -0,0 +1,14 @@
+import flask
+import random
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    n = random.randint(0,100)
+    print(n)
+    return str(n)
+
+app.run()
